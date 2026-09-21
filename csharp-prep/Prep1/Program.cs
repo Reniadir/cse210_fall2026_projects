81900673d40f8fddd4995d5b350b69895c94df2c
Reniadir <smedlogan@gmail.com>
2026-09-21 09:17:15 -0600

    Made variables predeclared

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        string first_name;
+        string last_name;
+
         Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
+        first_name = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
+        last_name = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
