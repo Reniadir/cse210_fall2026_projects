b8724f2b5fdada3df6e2953394ad8f39d8641a1d
Reniadir <smedlogan@gmail.com>
2026-09-23 09:08:51 -0600

    Fisnished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess;
+        bool escape = false;
+
+        do  
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess == number)
+            {
+                escape = true;
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (escape == false);
+        
+        Console.WriteLine("You guessed it!");
     }
 }
